035e53d733af14c95d4ef4042a58d36f5a5c9759
Charles Serrato <serrato.charles@gmail.com>
2021-03-23 18:21:22 +0800

    New Branch + Before ultimate refactoring

---
+++ Assets/Scripts/TestAbstract_SO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public abstract class TestAbstract_SO : ScriptableObject
+{
+    public float Value;
+}
