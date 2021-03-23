689f1d9990f2e889962c3c9b33a0ce89369a0363
Charles Serrato <serrato.charles@gmail.com>
2021-03-23 19:10:39 +0800

    Trying out primitive references as scriptable objects

---
+++ Assets/Scriptable Objects/_General Game Design/TestScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] FloatReference testFloat;
+    [SerializeField] IntReference testInt;
+
+    private void Start()
+    {
+        Debug.Log(testFloat.Value);
+        Debug.Log(testInt.Value);
+    }
+}
--- Assets/Scripts/TestAbstract_SO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public abstract class TestAbstract_SO : ScriptableObject
-{
-    public float Value;
-}
